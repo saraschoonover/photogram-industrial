f21dd60b0627ff98682d880a1f33e1872b2a7a42
Sara Schoonover <Saramschoon@gmail.com>
2023-10-11 21:45:30 +0000

    merged conflicts

--- db/migrate/20231011210427_create_photos.rb
+++ db/migrate/20231011210427_create_photos.rb
@@ -5,7 +5,7 @@ class CreatePhotos < ActiveRecord::Migration[7.0]
       t.integer :comments_count
       t.integer :likes_count, default: 0
       t.text :caption
-      t.references :owner, null: false, foreign_key: true
+      t.references :owner, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
