411efc6ca6c30008fd8806106920e87f1a843c29
Sara Schoonover <Saramschoon@gmail.com>
2023-10-16 15:05:19 +0000

    completed users profile page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   get ":username/liked" => "users#liked", as: :liked
  
   get "/:username" => "users#show", as: :user
-
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#following", as: :following
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   
