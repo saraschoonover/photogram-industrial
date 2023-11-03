b91be800f97381a7efa7f33c30dfb85a678724d0
Sara Schoonover <Saramschoon@gmail.com>
2023-11-03 09:57:54 -0500

    Revert "tabbed interface"

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,8 @@
 Rails.application.routes.draw do
-  # Defines the root path route ("/")
-  root "photos#index"
-
-  devise_for :users
-  
-  resources :likes
-  resources :follow_requests
-  resources :comments
   resources :photos
-  
-  get ":username/liked" => "users#liked", as: :liked
- 
-  get "/:username" => "users#show", as: :user
-  get ":username/feed" => "users#feed", as: :feed
-  get ":username/followers" => "users#followers", as: :followers
-  get ":username/following" => "users#following", as: :following
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  
+  # Defines the root path route ("/")
+   root "photos#index"
 end
--- db/migrate/20231012155701_add_default_to_private.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultToPrivate < ActiveRecord::Migration[7.0]
-  def change
-    change_column_default(
-      :users,
-      :private,
-      true
-    )
-  end
-end
